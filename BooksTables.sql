6df0502f926bdb6fc2fcf2165abf5477f4989b90
PRONOY <pronoybanik82@gmail.com>
2025-03-24 12:04:10 +0600

    average price

--- BooksTables.sql
+++ BooksTables.sql
@@ -88,9 +88,10 @@ HAVING
 
 -- 6:- Find the average price of books in the store.
 SELECT
-    avg(price)
+    ROUND(AVG(price), 2) AS avg_book_price
 FROM
     books;
+
 
 -- 7:- Query to increase the price of all books published before 2000 by 10%
 UPDATE
